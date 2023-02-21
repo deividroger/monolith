353325be2277a2702e6876922e45a60f4b635bd1
Deivid Santos <deivid@deividroger.net>
2023-02-21 07:31:42 -0300

    first commit

---
+++ src/modules/product-adm/facade/product-adm.facade.spec.ts
@@ -0,0 +1,51 @@
+import { Sequelize } from "sequelize-typescript";
+import ProductAdmFacadeFactory from "../factory/facade.factory";
+import { ProductModel } from "../repository/product.model";
+import ProductRepository from "../repository/product.repository";
+import AddProductUseCase from "../usecase/add-product/add-product.usecase";
+import ProductAdmFacade from "./product-adm.facade";
+
+describe("ProductAdmFacade test", () => {
+
+    let sequelize: Sequelize
+
+    beforeEach(async () => {
+        sequelize = new Sequelize({
+            dialect: 'sqlite',
+            storage: ':memory:',
+            logging: false,
+            sync: { force: true }
+        });
+
+        sequelize.addModels([ProductModel]);
+
+        await sequelize.sync();
+
+    });
+
+    it("should create product", async () => {
+
+        const productFacade = ProductAdmFacadeFactory.create();
+
+        const input = {
+            id: "1",
+            name: "Product 1",
+            description: "Product 1 description",
+            purchasePrice: 100,
+            stock: 10
+        }
+
+        await productFacade.addProduct(input);
+
+        const product = await ProductModel.findOne({ where: { id: input.id } });
+
+        expect(product).toBeDefined();
+        expect(product.id).toEqual(input.id);
+        expect(product.name).toEqual(input.name);
+        expect(product.description).toEqual(input.description);
+        expect(product.purchasePrice).toEqual(input.purchasePrice);
+        expect(product.stock).toEqual(input.stock);
+
+    });
+
+});
---
+++ src/modules/product-adm/factory/facade.factory.ts
@@ -0,0 +1,17 @@
+import ProductAdmFacade from "../facade/product-adm.facade";
+import ProductAdmFacadeInterface from "../facade/product-adm.facade.interface";
+import ProductRepository from "../repository/product.repository";
+import AddProductUseCase from "../usecase/add-product/add-product.usecase";
+
+export default class ProductAdmFacadeFactory{
+    
+    static create(): ProductAdmFacadeInterface{
+        const productRepository = new ProductRepository();
+        const addProductUseCase = new AddProductUseCase(productRepository);
+        const productFacade = new ProductAdmFacade({
+             addUseCase: addProductUseCase,
+            stockUseCase: undefined
+        });
+        return productFacade;
+    }
+}
