987a5ba22033bf984f9250015083760155935a71
Deivid Santos <deivid@deividroger.net>
2023-02-21 07:32:14 -0300

    added productAdm factory

--- src/modules/product-adm/factory/facade.factory.ts
+++ src/modules/product-adm/factory/facade.factory.ts
@@ -4,8 +4,9 @@ import ProductRepository from "../repository/product.repository";
 import AddProductUseCase from "../usecase/add-product/add-product.usecase";
 
 export default class ProductAdmFacadeFactory{
-    
+
     static create(): ProductAdmFacadeInterface{
+        
         const productRepository = new ProductRepository();
         const addProductUseCase = new AddProductUseCase(productRepository);
         const productFacade = new ProductAdmFacade({
